4cd36f4f53f153d78ed8bbf2b4126cac0757922b
d.zheludkov <d.zheludkov@deltasolutions.ru>
2022-05-08 19:31:21 +0300

    return server contextPath

--- src/main/java/kimoror/siam/rest/AuthController.java
+++ src/main/java/kimoror/siam/rest/AuthController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("/siam/auth")
+@RequestMapping("/auth")
 public class AuthController {
 
     private final AuthService authService;
